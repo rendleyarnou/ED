cedf4b9cca9983a6984f0999ee4102e6497b95da
RendleyArnou17 <37277102+RendleyArnou17@users.noreply.github.com>
2018-04-08 18:03:06 -0300

    Update main.cpp

--- 04_fractais/main.cpp
+++ 04_fractais/main.cpp
@@ -85,28 +85,33 @@
 
 /////////////////////////////////////////////////////////////////////////
 
-//FRACTAL DO TRIANGULO
+//FRACTAL DA CIRCULOS
 
 //#include <lib/pen.h>
-//void triangulo(Pen& p, int lado){
-//    if(lado < 5)
+//void circulos(Pen &p, int lado){
+//    if(lado < 1)
 //        return;
-//    for(int i = 0; i < 3; i++){
-//        p.setThickness(1);
-//        p.setColor(rand()%255, rand()%255, rand()%255);
+//    p.setColor(rand()%255, rand()%255 ,rand()%255);
+//    p.circle(lado);
+//    for(int i = 0; i < 6; i++){
+//        p.right(60);
+//        p.up();
+//        p.setColor(rand()%255, rand()%255 ,rand()%255);
 //        p.walk(lado);
-//        p.right(120);
-//        triangulo(p, lado/2);
+//        p.down();
+//        circulos(p, lado * 0.4 );
+//        p.up();
+//        p.walk(-lado);
+//        p.down();
 //    }
 //}
 //void fractal(){
-
 //    Pen p(800, 600);
-//    p.setXY(700, 580);
-//    p.setHeading(180);
-//    p.setSpeed(50);
-//    int lado = 600;
-//    triangulo(p, lado);
+//    p.setXY(400, 300);
+//    p.setHeading(90);
+//    p.setSpeed(500);
+//    int lado = 100;
+//    circulos(p, lado);
 //    p.wait();
 //}
 //int main(){
@@ -114,29 +119,33 @@
 //    return 0;
 //}
 
-/////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
-//FRACTAL DA ROTACAO
+//FRACTAL DA TRIGO
 
 //#include <lib/pen.h>
-//void embua(Pen& p, int lado){
-//    if(lado < 2)
+//void trigo(Pen &p, int lado){
+//    if(lado < 0.1)
 //        return;
-//    p.setColor(sf::Color::Blue);
-//    p.walk(lado);
-//    p.right(90);
-//    embua(p, lado - 2);
-//    p.left(90);
-//    p.setColor(sf::Color::Yellow);
-//    p.walk(-lado);
+//    for(int i = 0; i < 7; i++){
+//        p.setColor(rand()%255, rand()%255, rand()%255);
+//        p.walk(lado);
+//        p.right(45);
+//        trigo(p, lado/3.5);
+//        p.left(90);
+//        trigo(p, lado/3.5);
+//        p.right(45);
+//    }
+//    p.setColor(rand()%255, rand()%255, rand()%255);
+//    p.walk(-lado*7);
 //}
 //void fractal(){
 //    Pen p(800, 600);
-//    p.setXY(700, 590);
-//    p.setHeading(180);
-//    p.setSpeed(100);
-//    int lado = 580;
-//    embua(p, lado);
+//    p.setXY(400, 580);
+//    p.setHeading(90);
+//    p.setSpeed(0);
+//    int lado = 60;
+//    trigo(p, lado);
 //    p.wait();
 //}
 //int main(){
@@ -144,4 +153,47 @@
 //    return 0;
 //}
 
-/////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+
+//FRACTAL DA ESPIRAL
+
+// #include <lib/pen.h>
+// void espiral(Pen& p, int lado){
+//    if(lado < 5)
+//        return;
+//    for(int i = 0; i < 1; i++){
+//        p.setThickness(3);
+//        p.setColor(rand()%255, rand()%255, rand()%255);
+//        p.walk(lado);
+//        p.right(90);
+//        p.setColor(rand()%255, rand()%255, rand()%255);
+//        p.walk(lado);
+//        p.right(90);
+//        p.setColor(rand()%255, rand()%255, rand()%255);
+//        p.walk(lado);
+//        p.right(90);
+//        p.setColor(rand()%255, rand()%255, rand()%255);
+//        p.walk(lado);
+//        p.up();
+//        p.setColor(rand()%255, rand()%255, rand()%255);
+//        p.walk(lado);
+//        p.rotate(60);
+//        p.down();
+       
+//        espiral(p, lado - 5);
+//    }
+// }
+// void fractal(){
+//     Pen p(800, 600);
+//     p.setXY(500, 150);
+//     p.setHeading(90);
+//     p.setSpeed(100);
+//     int lado = 110;
+//     espiral(p, lado);
+//     p.wait();
+// }
+// int main(){
+//     fractal();
+//     return 0;
+// }
